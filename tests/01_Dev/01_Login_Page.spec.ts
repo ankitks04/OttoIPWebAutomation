d390e963f327f3d6ca44793797da394c36eb250f
Ankit Kumar Singh <ankitks04@example.com>
2026-08-07 15:04:07 +0530

    updated code

--- tests/01_Dev/01_Login_Page.spec.ts
+++ tests/01_Dev/01_Login_Page.spec.ts
@@ -1,17 +1,33 @@
 import { test, expect } from "@playwright/test";
 
+//test.setTimeout(120000);
+
 test("login test", async ({ page }) => {
     // 'page' is automatically created for you
     // Playwright already launched a browser, created a context, and opened this page
 
     await page.goto("https://filehistory.bhip-dev.com/login");
-    await page.fill("#username", "ottoip_test5@admin.com");
-    await page.fill("#password", "CAjboQMDYzMD");
-    await page.click("#login-btn");
 
-    let title = await page.title();
-    console.log("Title:", title);
+    await page.locator("#login-email").fill("ottoip_test5@admin.com");
+    // await page.fill("#username", "ottoip_test5@admin.com");
+
+    await page.locator("//input[@id='login-password']").fill("CAjboQMDYzMD");
+    // await page.fill("#password", "CAjboQMDYzMD");
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await page.getByText("Skip").click({
+        timeout: 80000,
+    });
 
     await expect(page).toHaveURL("https://filehistory.bhip-dev.com/");
-    console.log("URL verified ✅");
+    await expect(page).toHaveTitle("Otto IP");
+
+    await expect(page.getByText("OttoIPProfessional Plan")).toBeVisible({
+        timeout: 90000,
+    });
+
+    await page.pause();
+
+    console.log("URL verified âœ…");
 });
---
+++ tests/01_Dev/02_Workspace_creation.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from "@playwright/test";
+
+test.setTimeout(120000);
+
+test("login test", async ({ page }) => {
+    await page.goto("https://filehistory.bhip-dev.com/login");
+
+    await page.locator("#login-email").fill("ottoip_test5@admin.com");
+    // await page.fill("#username", "ottoip_test5@admin.com");
+
+    await page.locator("//input[@id='login-password']").fill("CAjboQMDYzMD");
+    // await page.fill("#password", "CAjboQMDYzMD");
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await page.getByText("Skip").click({
+        timeout: 80000,
+    });
+
+    //await page.locator("#root > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(2) > div > div:nth-of-type(1) > div:nth-of-type(2) > button").click({
+    // timeout: 80000,
+    //  });
+
+    await page.getByText("Dev-Ankit-testt9__Test39").click({
+        timeout: 6000,
+    });
+
+    await page.locator("button").nth(5).click({
+        timeout: 90000,
+    });
+
+    await page.getByRole("button", { name: "No" }).click();
+
+    await page.locator("#input-matter-number").fill("123456789");
+
+    await page.getByRole("combobox").click({
+        timeout: 70000,
+    });
+
+    await page.getByText("General").click({
+
+        //await page.locator("#radix-_r_nb_").click({
+        timeout: 70000,
+    });
+
+    const dialog = page.getByRole("dialog", { name: "Add Workspace" });
+    await dialog.getByRole("button", { name: "Save" }).click();
+
+    await page.pause();
+
+});
